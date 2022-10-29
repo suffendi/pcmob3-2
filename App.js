fb98c2c7b930d18533dc04de93a4ecccbff871a5
suffendi <104569090+suffendi@users.noreply.github.com>
2022-10-29 18:10:26 -0400

    mob3-2 to do app

--- App.js
+++ App.js
@@ -1,66 +1,28 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { StatusBar } from "expo-status-bar";
-import React, { useEffect } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React from "react";
+import { StyleSheet } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Entypo } from "@expo/vector-icons";
+import * as SQLite from "expo-sqlite";
+import NotesStack from "./screens/NotesStack";
+import AddScreen from "./screens/AddScreen";
 
-
-
-function NotesScreen({ navigation }) {
-  useEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <TouchableOpacity onPress={addNote} style={styles.headerIcon}>
-          <Entypo name="new-message" size={24} color="black" />
-        </TouchableOpacity>
-      ),
-    });
-  });
-
-  function addNote() {
-    console.log("Add Note");
-  }
-
-
- return <View style={styles.container}></View>;
-}
+const db = SQLite.openDatabase("notes.db");
 
 const Stack = createStackNavigator();
 
 export default function App() {
  return (
    <NavigationContainer>
-     <Stack.Navigator>
+     <Stack.Navigator mode="modal" headerMode="none">
        <Stack.Screen
-         name="Notes"
-         component={NotesScreen}
-         options={{
-           headerTitle: "Notes App",
-           headerTitleStyle: {
-             fontWeight: "bold",
-             fontSize: 30,
-           },
-           headerStyle: {
-             height: 120,
-             backgroundColor: "yellow",
-             borderBottomColor: "#ccc",
-             borderBottomWidth: 1,
-           },
-         }}
+         name="Notes Stack"
+         component={NotesStack}
+         options={{ headerShown: false }}
        />
+       <Stack.Screen name="Add Note" component={AddScreen} />
      </Stack.Navigator>
    </NavigationContainer>
  );
 }
-
-const styles = StyleSheet.create({
- container: {
-   flex: 1,
-   backgroundColor: "#ffc",
-   alignItems: "center",
-   justifyContent: "center",
- },
-});
 
 
